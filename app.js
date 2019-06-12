0555c9ad3b696ae8255c9064e1cf6f9b1bb31171
wjw@choicesoft.com.cn <p0063972>
2019-06-12 16:37:34 +0800

    add

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 new Vue({
-    el: '#vue-app',
-    data: {
-        available: false,
-        nearby: false
+  el: '#vue-app',
+  data: {
+    available: false,
+    nearby: false,
+    ObjectChange: {
+      test: true,
+      atest: false,
     },
-    methods: {
-
+    isActive: true,
+    position: '1',
+    TheStyle: {
+      backgroundColor: 'yellow',
+      width: '300px',
+      height: '60px',
+      transition: 'all 0.7s',
     },
-    computed: {
-        compClasses: function(){
-            return{
-                available: this.available,
-                nearby: this.nearby,
-            }
-        }
-    }
+  },
+  methods: {
+    changeColor() {
+      this.$data.TheStyle.backgroundColor = 'black';
+      this.$data.TheStyle.width = '50px';
+    },
+    changeColor2() {
+      this.$refs.TheStyle2.style.backgroundColor = 'red';
+      this.$refs.TheStyle2.style.width = '400px';
+      this.$refs.TheStyle2.style.transition = 'all 0.9s';
+    },
+  },
+  computed: {
+    compClasses() {
+      return {
+        available: this.available,
+        nearby: this.nearby
+      };
+    },
+  },
 });
